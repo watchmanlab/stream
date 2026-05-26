3f0033c21d92e5644597c48a19284709149a3bb6
smari sofiane <smari.sofiane@gmail.com>
2026-05-26 15:46:10 +0100

    dev

--- src/core/mitto.ts
+++ src/core/mitto.ts
@@ -6,7 +6,38 @@ export class Mitto<VALUE = void> {
   private _listenerRemoved?: Mitto<Mitto.Listener<VALUE>>;
   private _aborted?: Mitto<void>;
 
-  constructor(private options?: Mitto.Options<VALUE, Mitto<VALUE>>) {}
+  constructor(private options = {} as Mitto.Options<VALUE, Mitto<VALUE>>) {
+    if (options.scoop) {
+      if (options.scoop instanceof Mitto) {
+        options.scoop.aborted.next(() => this.abort());
+      } else if (options.scoop.any) {
+        options.scoop.any.forEach((other) => other.aborted.next(() => this.abort()));
+      } else {
+        let count = options.scoop.all.length;
+        options.scoop.all.forEach((other) => other.aborted.next(() => !count-- && this.abort()));
+      }
+    }
+
+    if (options?.source) {
+      const { listenerAdded, listenerRemoved, aborted } = options;
+
+      let abort: () => void;
+      this.options.listenerAdded = (fn, self) => {
+        if (this.listenersCount === 1) abort = options.source!(this);
+
+        listenerAdded?.(fn, self);
+      };
+      this.options.listenerRemoved = (fn, self) => {
+        if (this.listenersCount === 0) abort();
+
+        listenerRemoved?.(fn, self);
+      };
+      this.options.aborted = (self) => {
+        abort();
+        aborted?.(self);
+      };
+    }
+  }
 
   get listenersCount() {
     return this._listeners.size;
@@ -58,12 +89,22 @@ export class Mitto<VALUE = void> {
     this._aborted?.emit();
     this._aborted?.abort();
     this.options?.aborted?.(this);
-    this.options = this._listenerAdded = this._listenerRemoved = this._aborted = undefined;
+    this.options.aborted =
+      this.options.emited =
+      this.options.listenerAdded =
+      this.options.listenerRemoved =
+      this.options.source =
+      this._listenerAdded =
+      this._listenerRemoved =
+      this._aborted =
+        undefined;
   }
-  emit(value: VALUE) {
-    this.options?.emited?.(value, this);
-    for (const fn of this._listeners) {
-      fn(value);
+  emit(...values: [value: VALUE, ...values: VALUE[]]) {
+    for (const value of values) {
+      this.options?.emited?.(value, this);
+      for (const fn of this._listeners) {
+        fn(value);
+      }
     }
   }
   listen(fn: Mitto.Listener<VALUE>, options?: Mitto.ListenOptions): Mitto<void> {
@@ -74,7 +115,7 @@ export class Mitto<VALUE = void> {
     const abortSignal = new Mitto({
       emited: () => {
         abortSignal.abort();
-        this._listeners.delete(fn);
+        if (!this._listeners.delete(fn)) return;
         this.options?.listenerRemoved?.(fn, this);
         this._listenerRemoved?.emit(fn);
       },
@@ -95,131 +136,468 @@ export class Mitto<VALUE = void> {
     return this.map((v) => v);
   }
   map<MAPPED = VALUE>(fn: (value: VALUE) => MAPPED) {
-    let abortSignal: Mitto;
-    const mitto = new Mitto<MAPPED>({
-      aborted: (self) => {
-        abortSignal.emit();
-      },
-      listenerAdded: (_, self) => {
-        if (self.listenersCount > 1) return;
-        abortSignal = this.listen((value) => self.emit(fn(value)));
-      },
-      listenerRemoved: (_, self) => {
-        if (self.listenersCount > 0) return;
-        abortSignal.emit();
-      },
+    return new Mitto<MAPPED>({
+      scoop: this,
+      source: (self) => this.listen((value) => self.emit(fn(value))).emit.bind(self),
     });
-
-    this.aborted.next(() => {
-      mitto.abort();
-    });
-    return mitto;
   }
-  filter<FILTERED extends VALUE = VALUE>(fn: (value: VALUE) => value is FILTERED) {
-    const mitto = new Mitto<FILTERED>();
-
-    const abort = this.listen((value) => {
-      const result = fn(value);
-      if (result) mitto.emit(value);
+  filter<FILTERED extends VALUE = VALUE>(predicate: (value: VALUE) => value is FILTERED): Mitto<FILTERED>;
+  filter(predicate: (value: VALUE) => boolean): Mitto<VALUE>;
+  filter<FILTERED extends VALUE = VALUE>(predicate: (value: VALUE) => value is FILTERED) {
+    return new Mitto<FILTERED>({
+      scoop: this,
+      source: (self) =>
+        this.listen((value) => {
+          const result = predicate(value);
+          if (result) self.emit(value);
+        }).emit.bind(self),
     });
-
-    this.aborted.next(() => {
-      mitto.abort();
-      abort.emit();
-    });
-    return mitto;
   }
   merge<MITTOS extends [mitto: Mitto<any>, ...mittos: Mitto<any>[]]>(...mittos: MITTOS) {
-    const mitto = new Mitto<VALUE | Mitto.ExtractValue<MITTOS[number]>>();
-
-    const aborts = [this, ...mittos].map((m) => m.listen((value) => mitto.emit(value)));
-
-    this.aborted.next(() => {
-      mitto.abort();
-      aborts.forEach((abort) => abort.emit());
+    return new Mitto<VALUE | Mitto.ExtractValue<MITTOS[number]>>({
+      scoop: this,
+      source: (self) => {
+        const signals = [this, ...mittos].map((m) => m.listen((value) => self.emit(value)));
+        return () => signals.forEach((abort) => abort.emit());
+      },
     });
-    return mitto;
   }
   flat<DEPTH extends number = 0>(depth = 0 as DEPTH) {
-    const mitto = new Mitto<FlatArray<VALUE, DEPTH>>();
-    const abort = this.listen((value) => {
-      if (Array.isArray(value)) {
-        const flatten = depth === 0 ? value : value.flat(depth);
-        for (let i = 0, length = flatten.length; i < length; i++) {
-          mitto.emit(flatten[i]);
-        }
-      } else {
-        mitto.emit(value as never);
-      }
+    return new Mitto<FlatArray<VALUE, DEPTH>>({
+      scoop: this,
+      source: (self) =>
+        this.listen((value) => {
+          if (Array.isArray(value)) {
+            const flatten = depth === 0 ? value : value.flat(depth);
+            for (let i = 0, length = flatten.length; i < length; i++) {
+              self.emit(flatten[i]);
+            }
+          } else {
+            self.emit(value as never);
+          }
+        }).emit.bind(self),
     });
-
-    this.aborted.next(() => {
-      mitto.abort();
-      abort.emit();
-    });
-    return mitto;
   }
   group<SIZE extends number = 2>(size = 2 as SIZE) {
-    const mitto = new Mitto<Mitto.FixedArray<VALUE, SIZE>>();
     const buffer = new Array();
-    const abort = this.listen((value) => {
-      buffer.push(value);
 
-      if (buffer.length === size) {
-        const out = [...buffer];
-        buffer.length = 0;
-        mitto.emit(out as never);
-      }
-    });
+    return new Mitto<Mitto.FixedArray<VALUE, SIZE>>({
+      scoop: this,
+      source: (self) =>
+        this.listen((value) => {
+          buffer.push(value);
 
-    this.aborted.next(() => {
-      mitto.abort();
-      abort.emit();
+          if (buffer.length === size) {
+            const out = [...buffer];
+            buffer.length = 0;
+            self.emit(out as never);
+          }
+        }).emit.bind(self),
+      aborted: () => (buffer.length = 0),
     });
-    return mitto;
   }
   debounce(ms: number) {
-    const mitto = new Mitto<VALUE>();
     let timer: any = null;
+    return new Mitto<VALUE>({
+      scoop: this,
+      source: (self) => {
+        const signal = this.listen((value) => {
+          clearTimeout(timer);
+          timer = setTimeout(() => self.emit(value), ms);
+        });
 
-    const abort = this.listen((value) => {
-      clearTimeout(timer);
-      timer = setTimeout(() => mitto.emit(value), ms);
+        return () => {
+          signal.emit();
+          clearTimeout(timer);
+        };
+      },
+      aborted: () => clearTimeout(timer),
     });
-    this.aborted.next((value) => {
-      mitto.abort();
-      clearTimeout(timer);
-      abort.emit();
-    });
-    return mitto;
   }
   throttle(ms: number) {
-    const mitto = new Mitto<VALUE>();
     let inThrottle = false;
     let timer: any = null;
 
-    const abort = this.listen((value) => {
-      if (inThrottle) return;
+    return new Mitto<VALUE>({
+      scoop: this,
+      source: (self) => {
+        const signal = this.listen((value) => {
+          if (inThrottle) return;
 
-      mitto.emit(value);
-      inThrottle = true;
+          self.emit(value);
+          inThrottle = true;
 
-      timer = setTimeout(() => {
-        inThrottle = false;
-      }, ms);
+          timer = setTimeout(() => {
+            inThrottle = false;
+          }, ms);
+        });
+
+        return () => {
+          signal.emit();
+          clearTimeout(timer);
+        };
+      },
+      aborted: () => clearTimeout(timer),
     });
-
-    this.aborted.next(() => {
-      mitto.abort();
-      clearTimeout(timer);
-      abort.emit();
+  }
+  take(count: number) {
+    return new Mitto<VALUE>({
+      scoop: this,
+      source: (self) =>
+        this.listen((value) => {
+          self.emit(value);
+          if (--count === 0) self.abort();
+        }).emit.bind(self),
     });
-    return mitto;
+  }
+  takeWhile(fn: (value: VALUE) => boolean) {
+    return new Mitto<VALUE>({
+      scoop: this,
+      source: (self) =>
+        this.listen((value) => {
+          if (!fn(value)) {
+            self.abort();
+            return;
+          }
+
+          self.emit(value);
+        }).emit.bind(self),
+    });
+  }
+  takeUntil(fn: (value: VALUE) => boolean) {
+    return new Mitto<VALUE>({
+      scoop: this,
+      source: (self) =>
+        this.listen((value) => {
+          if (fn(value)) {
+            self.abort();
+            return;
+          }
+
+          self.emit(value);
+        }).emit.bind(self),
+    });
+  }
+  skip(count: number) {
+    return new Mitto<VALUE>({
+      scoop: this,
+      source: (self) =>
+        this.listen((value) => {
+          if (--count > 0) return;
+          self.emit(value);
+        }).emit.bind(self),
+    });
+  }
+  skipWhile(fn: (value: VALUE) => boolean) {
+    return new Mitto<VALUE>({
+      scoop: this,
+      source: (self) =>
+        this.listen((value) => {
+          if (fn(value)) return;
+
+          self.emit(value);
+        }).emit.bind(self),
+    });
+  }
+  skipUntil(fn: (value: VALUE) => boolean) {
+    return new Mitto<VALUE>({
+      scoop: this,
+      source: (self) =>
+        this.listen((value) => {
+          if (!fn(value)) return;
+
+          self.emit(value);
+        }).emit.bind(self),
+    });
+  }
+  signal() {
+    return new Mitto({
+      scoop: this,
+      source: (self) =>
+        this.next(() => {
+          self.emit();
+          self.abort();
+        }).emit.bind(self),
+    });
+  }
+  distinct() {
+    let last: VALUE;
+    return new Mitto<VALUE>({
+      scoop: this,
+      source: (self) =>
+        this.listen((value) => {
+          if (value !== last) {
+            last = value;
+            self.emit(value);
+          }
+        }).emit.bind(self),
+    });
+  }
+  distinctBy<KEY>(fn: (value: VALUE) => KEY) {
+    let last: KEY;
+    return new Mitto<VALUE>({
+      scoop: this,
+      source: (self) =>
+        this.listen((value) => {
+          const key = fn(value);
+          if (key !== last) {
+            last = key;
+            self.emit(value);
+          }
+        }).emit.bind(self),
+    });
+  }
+  scan<ACC>(seed: ACC, fn: (acc: ACC, value: VALUE) => ACC) {
+    let acc = seed;
+    return new Mitto<ACC>({
+      scoop: this,
+      source: (self) =>
+        this.listen((value) => {
+          acc = fn(acc, value);
+          self.emit(acc);
+        }).emit.bind(self),
+    });
+  }
+  delay(ms: number) {
+    let timer: any = null;
+    return new Mitto<VALUE>({
+      scoop: this,
+      source: (self) =>
+        this.listen((value) => {
+          timer = setTimeout(() => self.emit(value), ms);
+        }).emit.bind(self),
+      aborted: () => clearTimeout(timer),
+    });
+  }
+  auditTime(ms: number) {
+    let timer: any = null;
+    let latest: VALUE;
+    return new Mitto<VALUE>({
+      scoop: this,
+      source: (self) => {
+        const signal = this.listen((value) => {
+          latest = value;
+          if (!timer) {
+            timer = setTimeout(() => {
+              self.emit(latest);
+              timer = null;
+            }, ms);
+          }
+        });
+        return () => {
+          signal.emit();
+          clearTimeout(timer);
+        };
+      },
+      aborted: () => clearTimeout(timer),
+    });
+  }
+  combineLatest<OTHERS extends readonly [other: Mitto.AnyMitto, ...others: Mitto.AnyMitto[]]>(...others: OTHERS) {
+    const mittos = [this, ...others] as [other: Mitto.AnyMitto, ...others: Mitto.AnyMitto[]];
+
+    const EMPTY = Symbol.for("EMPTY");
+
+    const values = new Array(others.length + 1).fill(EMPTY);
+
+    return new Mitto<[curr: VALUE, ...{ [K in keyof OTHERS]: Mitto.ExtractValue<OTHERS[K]> }]>({
+      scoop: { any: mittos },
+      source: (self) => {
+        const signals = mittos.map((mitto, index) =>
+          mitto.listen((value) => {
+            values[index] = value;
+
+            if (values.every((v) => v !== EMPTY)) self.emit([...values] as never);
+          }),
+        );
+
+        return () => signals.forEach((signal) => signal.emit());
+      },
+
+      aborted: () => {
+        mittos.length = 0;
+        values.length = 0;
+      },
+    });
+  }
+  withLatestFrom<
+    OTHER extends Mitto.AnyMitto,
+    OTHER_VALUE extends Mitto.ExtractValue<OTHER> = Mitto.ExtractValue<OTHER>,
+  >(other: OTHER) {
+    let lastOther: OTHER_VALUE | Mitto.Empty;
+
+    return new Mitto<[curr: VALUE, other: OTHER_VALUE]>({
+      scoop: { any: [this, other] },
+      source: (self) => {
+        const s1 = other.listen((v) => {
+          lastOther = v;
+        });
+        const s2 = this.listen((v) => {
+          if (lastOther !== Mitto.EMPTY) self.emit([v, lastOther]);
+        });
+        return () => {
+          s1.emit();
+          s2.emit();
+        };
+      },
+    });
+  }
+  tap(fn: (value: VALUE) => void) {
+    return new Mitto<VALUE>({
+      scoop: this,
+      source: (self) =>
+        this.listen((value) => {
+          fn(value);
+          self.emit(value);
+        }).emit.bind(self),
+    });
+  }
+  startWith(...values: [value: VALUE, ...values: VALUE[]]) {
+    return new Mitto<VALUE>({
+      scoop: this,
+      source: (self) => {
+        for (const value of values) self.emit(value);
+        return this.listen((v) => self.emit(v)).emit.bind(self);
+      },
+    });
+  }
+  pairwise() {
+    let prev: VALUE | Mitto.Empty;
+    return new Mitto<[prev: VALUE, curr: VALUE]>({
+      scoop: this,
+      source: (self) =>
+        this.listen((value) => {
+          if (prev !== Mitto.EMPTY) self.emit([prev, value]);
+          prev = value;
+        }).emit.bind(self),
+    });
+  }
+  first(predicate?: (value: VALUE) => boolean) {
+    return new Mitto<VALUE>({
+      scoop: this,
+      source: (self) =>
+        this.listen((value) => {
+          if (!predicate || predicate(value)) {
+            self.emit(value);
+            self.abort();
+          }
+        }).emit.bind(self),
+    });
+  }
+  last() {
+    let latest: VALUE;
+    return new Mitto<VALUE>({
+      scoop: this,
+      source: (self) => {
+        const signal = this.listen((value) => (latest = value));
+        this.aborted.next(() => {
+          self.emit(latest);
+          self.abort();
+        });
+        return signal.emit.bind(signal);
+      },
+    });
+  }
+  catchError(fn: (error: any) => VALUE) {
+    return new Mitto<VALUE>({
+      scoop: this,
+      source: (self) =>
+        this.listen((value) => {
+          try {
+            self.emit(value);
+          } catch (error) {
+            self.emit(fn(error));
+          }
+        }).emit.bind(self),
+    });
+  }
+  sample(notifier: Mitto<any>) {
+    let latest: VALUE | Mitto.Empty;
+
+    return new Mitto<VALUE>({
+      scoop: { any: [this, notifier] },
+      source: (self) => {
+        const s1 = this.listen((v) => {
+          latest = v;
+        });
+        const s2 = notifier.listen(() => {
+          if (latest !== Mitto.EMPTY) self.emit(latest);
+        });
+        return () => {
+          s1.emit();
+          s2.emit();
+        };
+      },
+    });
+  }
+  bufferTime(ms: number) {
+    const buffer: VALUE[] = [];
+    let timer: any = null;
+
+    return new Mitto<VALUE[]>({
+      scoop: this,
+      source: (self) => {
+        const signal = this.listen((value) => {
+          buffer.push(value);
+
+          if (!timer) {
+            timer = setTimeout(() => {
+              if (buffer.length > 0) {
+                self.emit([...buffer]);
+                buffer.length = 0;
+              }
+              timer = null;
+            }, ms);
+          }
+        });
+
+        return () => {
+          signal.emit();
+          clearTimeout(timer);
+        };
+      },
+      aborted: () => {
+        clearTimeout(timer);
+        buffer.length = 0;
+      },
+    });
+  }
+  bufferCount(size: number, startBufferEvery = size) {
+    const buffers: VALUE[][] = [];
+    let count = 0;
+
+    return new Mitto<VALUE[]>({
+      scoop: this,
+      source: (self) =>
+        this.listen((value) => {
+          if (count % startBufferEvery === 0) {
+            buffers.push([]);
+          }
+
+          for (const buffer of buffers) {
+            buffer.push(value);
+            if (buffer.length === size) {
+              self.emit([...buffer]);
+              buffers.shift();
+            }
+          }
+
+          count++;
+        }).emit.bind(self),
+      aborted: () => {
+        buffers.length = 0;
+        count = 0;
+      },
+    });
   }
 }
 
 export namespace Mitto {
   export type Options<VALUE, SELF extends Mitto<VALUE>> = {
+    scoop?:
+      | Mitto.AnyMitto
+      | { any: [other: Mitto.AnyMitto, ...others: Mitto.AnyMitto[]]; all?: never }
+      | { all: [other: Mitto.AnyMitto, ...others: Mitto.AnyMitto[]]; any?: never };
+    source?: (self: SELF) => () => void;
     emited?: (value: VALUE, self: SELF) => void;
     aborted?: (self: SELF) => void;
     listenerAdded?: (fn: Listener<VALUE>, self: SELF) => void;
@@ -232,21 +610,19 @@ export namespace Mitto {
   export type FixedArray<VALUE, SIZE extends number = 2, ARR extends Array<VALUE> = []> = ARR["length"] extends SIZE
     ? ARR
     : FixedArray<VALUE, SIZE, [...ARR, VALUE]>;
+
+  export const EMPTY = Symbol.for("EMTY");
+  export type Empty = typeof EMPTY;
 }
 
-const m = new Mitto<number[][]>();
+const m1 = new Mitto<number>();
+const m2 = new Mitto<string>();
+const m3 = new Mitto<boolean>();
 
-m.flat(1).group().listen(console.log);
+const v = m1.combineLatest(m3, m2).listen((v) => console.log(v));
 
-const grouped = m.flat(1).group();
+m1.emit(1);
+m2.emit("ok");
+m3.emit(true);
 
-(async () => {
-  for await (const value of grouped) {
-    console.log("gen", value);
-  }
-})();
-
-m.emit([
-  [1, 2],
-  [3, 4],
-]);
+m1.pairwise().listen((v) => v);
