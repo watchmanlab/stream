f7d87b319200d7afab98323643db715805fceeed
smari sofiane <smari.sofiane@gmail.com>
2026-09-05 16:15:42 +0100

    test: add tests for  fromEventTarget source

--- src/sources/event-target-source.test.ts
+++ src/sources/event-target-source.test.ts
@@ -1,6 +1,7 @@
 import { describe, it, expect, mock } from "bun:test";
 import { fromEventTarget } from "./event-target-source";
 import { listen } from "../transformers/listen";
+import { Stream } from "../core/stream";
 
 describe("fromAbortSignal", () => {
   it("emit when event is dispateched by the event target", () => {
@@ -16,5 +17,23 @@ describe("fromAbortSignal", () => {
 
     expect(fn).toHaveBeenCalledWith(event);
     expect(eventType).toBe("click");
+  });
+
+  it("should not emit when source is terminated", () => {
+    const eventTarget = new EventTarget();
+    let eventType = "";
+
+    const fn = mock((e: Event) => (eventType = e.type));
+
+    const stream = Stream.from(fromEventTarget(eventTarget, "click"));
+    stream.pipe(listen(fn));
+
+    stream.terminate("abort");
+
+    const event = new Event("click");
+    eventTarget.dispatchEvent(event);
+
+    expect(fn).not.toHaveBeenCalled();
+    expect(eventType).toBe("");
   });
 });
